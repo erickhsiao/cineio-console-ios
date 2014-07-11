2d4860a4eefff0af66b0698c06f570e2ab4cb2f0
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2014-07-10 17:36:47 -0700

    initial commit (with cine.io identity icons / logomark)

---
+++ Console/Console/CineAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CineAppDelegate.h
+//  Console
+//
+//  Created by Jeffrey Wescott on 7/10/14.
+//  Copyright (c) 2014 cine.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CineAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Console/Console/CineViewController.h
@@ -0,0 +1,13 @@
+//
+//  CineViewController.h
+//  Console
+//
+//  Created by Jeffrey Wescott on 7/10/14.
+//  Copyright (c) 2014 cine.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CineViewController : UIViewController
+
+@end
